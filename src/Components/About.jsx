090349fc08f53c48263a333a254fd02b6bd5ff75
Aanya Singh Dhaka <122288528+aanyasinghdhaka@users.noreply.github.com>
2024-03-27 17:13:42 +0530

    Update About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -10,13 +10,14 @@ import React from "react";
  * About background image
  *
  * Below is a sample image. Upload the image of your choice into the "images"
+ // eslint-disable-next-line prettier/prettier
  * directory and import here for use. Then, set imageAltText to string that 
  * represents what you see in that image.
  *
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/pawel-czerwinski-qzaIDFtzcZ0-unsplash.jpg";
 
 const imageAltText = "purple and blue abstract background";
 
@@ -24,19 +25,19 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "Hello! I'm a IT student at IGDTUW with a passion for Web Development, Machine Learning, and community service. I enjoy turning ideas into code and am excited about the transformative power of technology.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
+  "Web development",
+  "Machine learning",
+  "Team building",
+  "Web Design",
   "Mobile user interfaces",
-  "Graphic design",
+  "Community Service",
 ];
 
 /**
